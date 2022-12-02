349fe7f0919e29999a1cca54f6c5894a96752561
Alex <aleksei.golovanov@simbirsoft.com>
2022-12-02 16:47:56 +0300

    added variables

---
+++ src/main/java/com.simbirsoft.Golovanov/MayHoumAnimals.java
@@ -0,0 +1,20 @@
+package src.main.java.com.simbirsoft.Golovanov;
+
+public class MayHoumAnimals {
+
+    int age;
+    String Breed;
+    String Color;
+    int Weight;
+    String owner;
+
+    String[] Feeds = {"Seed", "Hay", "milk", "meat"};
+
+
+
+
+
+
+
+
+}
